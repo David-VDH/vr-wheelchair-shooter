1241cd34162bda4c1eda505922331424ec08a8f2
Tero Korpela <tero.korpela@gmail.com>
2024-04-22 22:26:31 +0300

    Camera offset reset functionality

--- Assets/Scripts/Utilities/VRWC_FollowTransform.cs
+++ Assets/Scripts/Utilities/VRWC_FollowTransform.cs
@@ -8,7 +8,9 @@ public class VRWC_FollowTransform : MonoBehaviour
 {
     [Tooltip("Transform of the rigidbody to follow.")]
     public Transform target;
+    public new Transform camera;
     Vector3 offset;
+    Vector3 cameraOffset;
 
     void Start()
     {
@@ -17,9 +19,22 @@ public class VRWC_FollowTransform : MonoBehaviour
 
     void Update()
     {
-        Vector3 rotatedOffset = target.localRotation * offset;
+        Vector3 rotatedOffset = target.localRotation * (offset - cameraOffset);
         transform.localPosition = target.localPosition + rotatedOffset;
 
         transform.rotation = target.rotation;
+    }
+
+    [ContextMenu("Reset Camera Offset")]
+    public void ResetCameraOffset()
+    {
+        cameraOffset = camera.localPosition;
+        cameraOffset.y = 0;
+    }
+
+    private void OnValidate()
+    {
+        if (camera == null)
+            camera = GetComponentInChildren<Camera>().transform;
     }
 }
